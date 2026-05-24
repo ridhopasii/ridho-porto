f9d9e06a429b0d7cf608e21151a82abb236f817e
Ridho Robbi Pasi <ridhorobbipasi@gmail.com>
2026-05-24 18:36:21 +0700

    fix: prevent supabase client from throwing at build time due to missing env variables

--- common/libs/supabase-server.ts
+++ common/libs/supabase-server.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // Use this for fetching data securely on server components
 export const supabaseServer = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://dummy.supabase.co",
+  process.env.SUPABASE_SERVICE_ROLE_KEY || "dummy_key"
 );
