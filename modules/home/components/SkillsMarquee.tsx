5ff29ad1af8041e0427c5bcad0ba54f0fda20b8a
Ridho Robbi Pasi <ridhorobbipasi@gmail.com>
2026-05-25 17:16:50 +0700

    style: remove gradient overlays from skills marquee as requested

--- modules/home/components/SkillsMarquee.tsx
+++ modules/home/components/SkillsMarquee.tsx
@@ -47,9 +47,6 @@ export default function SkillsMarquee({ skills }: SkillsMarqueeProps) {
 
   return (
     <div className="relative w-full overflow-hidden py-4 space-y-4">
-      {/* Left/Right fading gradient overlays */}
-      <div className="pointer-events-none absolute left-0 top-0 bottom-0 w-16 bg-gradient-to-r from-neutral-50 to-transparent dark:from-neutral-950 z-10" />
-      <div className="pointer-events-none absolute right-0 top-0 bottom-0 w-16 bg-gradient-to-l from-neutral-50 to-transparent dark:from-neutral-950 z-10" />
 
       {/* Row 1: Leftward */}
       <Marquee speed={35} direction="left" pauseOnHover={true} gradient={false}>
